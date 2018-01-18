3cca8b9cec96480fea804016d54a56002a0a2762
unknown <rian.triyana@RIATRI0NOTWIN0.iconpln.co.id>
2018-01-18 21:01:13 +0700

    menambahkan form rute tpi belum bisa tampil

---
+++ WindowsFormsApplication27/FormRute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication27
+{
+    public partial class FormRute : Form
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost; UID=root; Pwd=; database=database");
+        public FormRute()
+        {
+            InitializeComponent();
+        try
+        {
+            conn.Open();
+            initData();
+        }
+             catch (Exception ex)
+        {
+            MessageBox.Show("Failed with error : " + ex.Message);
+        }
+        }
+
+        private void initData()
+        {
+            string query = "Select * from rute";
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+            {
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+
+        private void FormRute_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
