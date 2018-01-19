bc1d521899add13c524afcbd61fecc641f162fa2
unknown <rian.triyana@RIATRI0NOTWIN0.iconpln.co.id>
2018-01-19 13:40:07 +0700

    Menambahkan fungsi tambah, ubah dan hapus pada form rute

--- WindowsFormsApplication27/FormRute.Designer.cs
+++ WindowsFormsApplication27/FormRute.Designer.cs
@@ -40,14 +40,14 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(329, 139);
+            this.button1.Location = new System.Drawing.Point(329, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -108,10 +108,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(38, 182);
+            this.dataGridView1.Location = new System.Drawing.Point(38, 195);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(481, 135);
+            this.dataGridView1.Size = new System.Drawing.Size(481, 122);
             this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label5
             // 
@@ -131,6 +132,7 @@
             this.button2.TabIndex = 11;
             this.button2.Text = "Ubah";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -140,30 +142,29 @@
             this.button3.TabIndex = 12;
             this.button3.Text = "Hapus";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // comboBox1
+            // textBox3
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(329, 68);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(109, 21);
-            this.comboBox1.TabIndex = 13;
+            this.textBox3.Location = new System.Drawing.Point(329, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
             // 
-            // comboBox2
+            // textBox4
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(329, 112);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(109, 21);
-            this.comboBox2.TabIndex = 14;
+            this.textBox4.Location = new System.Drawing.Point(329, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
             // 
             // FormRute
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 342);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
@@ -199,7 +200,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- WindowsFormsApplication27/FormRute.cs
+++ WindowsFormsApplication27/FormRute.cs
@@ -48,7 +48,7 @@ namespace WindowsFormsApplication27
         private void button1_Click(object sender, EventArgs e)
         {
             string query = "INSERT INTO rute (id, nama_rute, halte_asal, halte_tujuan) VALUES('" + textBox1.Text.ToString() + "', '" + textBox2.Text.ToString() + "'" +
-                    ", '" + comboBox1.GetItemText(this.comboBox1.SelectedItem) + "' , '" +comboBox1.GetItemText(this.comboBox1.SelectedItem)+ "' )";
+                    ", '" + textBox3.Text.ToString() + "' , '" + textBox4.Text.ToString() + "' )";
             //create command and assign the query and connection from the constructor
             MySqlCommand cmd = new MySqlCommand(query, conn);
 
@@ -59,7 +59,59 @@ namespace WindowsFormsApplication27
                 MessageBox.Show("Data berhasil ditambahkan");
                 textBox1.Text = "";
                 textBox2.Text = "";
-                comboBox1.SelectedItem = 0;
+                textBox3.Text = "";
+                textBox4.Text = "";
+                initData();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Gagal : " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "Update rute set id= '" + textBox1.Text.ToString() + "', halte_asal= '" + textBox3.Text.ToString() + "', halte_tujuan= '" + textBox4.Text.ToString() + "' Where nama_rute='" + textBox2.Text.ToString() + "'";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data berhasil diubah");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+               
+                initData();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Gagal : " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            textBox1.Text = row.Cells["id"].Value.ToString();
+            textBox2.Text = row.Cells["nama_rute"].Value.ToString();
+            textBox3.Text = row.Cells["halte_asal"].Value.ToString();
+            textBox4.Text = row.Cells["halte_tujuan"].Value.ToString();
+            //comboBox1.SelectedIndex = comboBox1.FindStringExact(tipe);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = "delete from rute Where id ='" + textBox1.Text.ToString() + "'";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data berhasil dihapus");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
                 initData();
             }
             catch (MySqlException ex)
